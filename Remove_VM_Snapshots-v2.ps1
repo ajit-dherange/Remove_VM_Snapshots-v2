270d425c3a3f10d1412ccc841b4324a7ae685cd0
Ajit Dherange <45383984+ajit-dherange@users.noreply.github.com>
2022-10-13 00:31:38 +0800

    Add files via upload

---
+++ Remove_VM_Snapshots-v2.ps1
@@ -0,0 +1,15 @@
+﻿
+# Add-PSSnapin VMware.VimAutomation.Core
+# $vcenter = XXXX
+# Connecting to vCenter
+# Connect-VIServer -Server $vcenter -User administrator@vsphere.local -Password ZZZZZ | out-null
+$snapshots  = Get-VM | Get-Snapshot
+foreach ($snap_data in $snapshots) {
+        if($snap_data.Created -lt (Get-Date).AddDays(-3))
+        {
+        Write-host $snap_data.VM "," $snap_data.Created "," $snap_data.Id | FT -AutoSize
+        #Get-VM $snap_data.VM | Get-Snapshot |  Remove-Snapshot -Confirm:$false -RemoveChildren
+        Start-Sleep -s 5
+        }
+    }
+
